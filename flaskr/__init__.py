ed62f0c4cd90b87bc4e7730e5acb358a70b4e48a
Hi-Im-Simon <szymol1001@gmail.com>
2021-11-05 13:51:01 +0100

    basic flask setup

---
+++ flaskr/__init__.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import Flask
+
+
+def create_app():
+    # create the app
+    # instance_relative_config tells the app that configuration files are relative to the instance folder
+    app = Flask(__name__, instance_relative_config=True)
+
+    # configurate the app
+    app.config.from_mapping(
+        # secret key to keep data safe, should be changed later
+        SECRET_KEY='dev',
+        # path to the database
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    @app.route('/')
+    def page_default():
+        return 'hewofddd'
+
+    @app.route('/eo')
+    def page_eo():
+        return 'harambe'
+
+    # initializes database's functionalities
+    from . import db
+    db.init_app(app)
+
+    return app
