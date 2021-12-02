8a3829aa65b782c101be8a8a37c1a72a2dd3d62c
Angelfrost <64253119+Angelfrost@users.noreply.github.com>
2021-12-02 13:02:08 +0100

    vaccine rollout (work in progress)

--- new_model.py
+++ new_model.py
@@ -5,47 +5,55 @@ S = [100000]
 I = [49]
 R = [0]
 D = [0]
-N = [S[-1]+I[-1]+R[-1]]
+V = [0]
+N = [S[-1]+I[-1]+R[-1]+V[-1]]
 
 infection_rate = 1/4
 recovery_rate = 1/12
 mortality_rate = 2/100*recovery_rate
 immunity_loss_rate = 1/90
-mask_rate = 0.5
+mask_rate = 0.1
 social_distancing_rate = 0.02
 mask_effectiveness = 0.5
 social_distancing_effectiveness = 0.85
+vaccination_rate = 0
+vax_immunity_loss_rate = 1
 
 R0 = (infection_rate*(1-mask_effectiveness*mask_rate)*(1-social_distancing_effectiveness*social_distancing_rate))/(mortality_rate+recovery_rate)
 
 T_end = 365
 
 for t in range(1, T_end):
-    if (t == 34):
-        social_distancing_rate = 0.95
-        R0 = (infection_rate*(1-mask_effectiveness*mask_rate)*(1-social_distancing_effectiveness*social_distancing_rate))/(mortality_rate+recovery_rate)
-        print(R0)
-    if (t == 100):
-        social_distancing_rate = 0.01
-        mask_rate = 0
-        R0 = (infection_rate*(1-mask_effectiveness*mask_rate)*(1-social_distancing_effectiveness*social_distancing_rate))/(mortality_rate+recovery_rate)
-        print(R0)
-    dS = -1 * infection_rate*(1-mask_effectiveness*mask_rate)*(1-social_distancing_effectiveness*social_distancing_rate) * (I[-1]*S[-1])/N[-1]+immunity_loss_rate*R[-1]
-    dI = infection_rate*(1-mask_effectiveness*mask_rate)*(1-social_distancing_effectiveness*social_distancing_rate) * (I[-1]*S[-1])/N[-1] - (recovery_rate+mortality_rate)*I[-1]
+    if t == 60:
+        vaccination_rate = 8/1000
+    # if (t == 34):
+    #     social_distancing_rate = 0.95
+    #     R0 = (infection_rate*(1-mask_effectiveness*mask_rate)*(1-social_distancing_effectiveness*social_distancing_rate))/(mortality_rate+recovery_rate)
+    #     print(R0)
+    # if (t == 100):
+    #     social_distancing_rate = 0.01
+    #     mask_rate = 0
+    #     R0 = (infection_rate*(1-mask_effectiveness*mask_rate)*(1-social_distancing_effectiveness*social_distancing_rate))/(mortality_rate+recovery_rate)
+    #     print(R0)
+    dS = -1 * infection_rate*(1-mask_effectiveness*mask_rate)*(1-social_distancing_effectiveness*social_distancing_rate) * (I[-1]*S[-1])/N[-1]+immunity_loss_rate*R[-1] - vaccination_rate*S[-1]
+    dI = infection_rate*(1-mask_effectiveness*mask_rate)*(1-social_distancing_effectiveness*social_distancing_rate) * (I[-1]*S[-1])/N[-1] + vax_immunity_loss_rate*infection_rate*(1-mask_effectiveness*mask_rate)*(1-social_distancing_effectiveness*social_distancing_rate)*(V[-1]/N[-1]) - (recovery_rate+mortality_rate)*I[-1]
     dR = recovery_rate*I[-1]-immunity_loss_rate*R[-1]
     dD = mortality_rate*I[-1]
+    dV = vaccination_rate*S[-1] - vax_immunity_loss_rate*infection_rate*(1-mask_effectiveness*mask_rate)*(1-social_distancing_effectiveness*social_distancing_rate)*(V[-1]/N[-1])
 
     S.append(S[-1] + dS)
     I.append(I[-1] + dI)
     R.append(R[-1] + dR)
     D.append(D[-1] + dD)
-    N.append(S[-1]+I[-1]+R[-1])
+    V.append(V[-1] + dV)
+    N.append(S[-1]+I[-1]+R[-1]+V[-1])
 
 fig, ax = plt.subplots()
 ax.plot([t for t in range(T_end)], S)
 ax.plot([t for t in range(T_end)], I)
 ax.plot([t for t in range(T_end)], R)
 ax.plot([t for t in range(T_end)], D)
+ax.plot([t for t in range(T_end)], V)
 ax.plot([t for t in range(T_end)], N)
 ax.grid()
 plt.show()
