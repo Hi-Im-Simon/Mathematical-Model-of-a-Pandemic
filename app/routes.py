d0b8600ae578d4ef0b3db263477fa67b1e7738e3
Angelfrost <64253119+Angelfrost@users.noreply.github.com>
2021-12-16 13:14:50 +0100

    English localisation

--- app/routes.py
+++ app/routes.py
@@ -36,7 +36,7 @@ def page_default():
         # or if the page is reloaded, send data to the template and display it
         return render_template(
             'main.html',
-            title = 'Main page — A Mathematical Model of COVID-19 Pandemic',
+            title = 'Main page — A Mathematical Model of a Pandemic',
             values = values,
             columns = columns,
         )
