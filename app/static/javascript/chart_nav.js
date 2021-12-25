aeae59f7e9dca702a0eb6347088825182bfa7678
Hi-Im-Simon <75808585+Hi-Im-Simon@users.noreply.github.com>
2021-12-25 19:22:57 +0100

    code cleanup

--- app/static/javascript/chart_nav.js
+++ app/static/javascript/chart_nav.js
@@ -1,23 +1,15 @@
 function switch_chart(dest) {
-    if (dest == 'left') {
-        if (data_set_i > 0)
-            data_set_i -= 1;
-        else
-            return;
+    if (dest == 'left' && data_set_i > 0) {
+        data_set_i -= 1;
     }
-    else if (dest == 'right') {
-        if (data_set_i < db['id'].length - 1)
-            data_set_i += 1;
-        else
-            return;
+    else if (dest == 'right' && data_set_i < db['id'].length - 1) {
+        data_set_i += 1;
     }
-    else
-        return;
+    else return;
     
     let data_set = new Map();
-    for (entry in db) {
+    for (entry in db)
         data_set.set(entry, db[entry][data_set_i]);
-    }
     let x_values = Array.from(Array(+(data_set.get('time')) + 1).keys());
     generateChart(chart_name='chart_2', action='update', data=[x_values, calc(data_set)]);
     document.getElementById('chart_id').innerHTML = data_set_i + 1;
--- app/static/javascript/math.js
+++ app/static/javascript/math.js
@@ -1,14 +1,14 @@
 function calc(data) {
-    Tp = 1;  // sampling interval in days, assume default = 1 day
+    let Tp = 1;  // sampling interval in days, assume default = 1 day
 
-    S = 1368048;  // susceptible compartment
-    I = 50;  // infected compartment
-    R = 0;  // recovered compartment
-    D = 0;  // deceased compartment
-    V = 0;  // vaccinated compartment
+    let S = 1368048;  // susceptible compartment
+    let I = 50;  // infected compartment
+    let R = 0;  // recovered compartment
+    let D = 0;  // deceased compartment
+    let V = 0;  // vaccinated compartment
 
-    N_0 = S + I + R + D + V;  // max initial population size
-    N = S + I + R + V;  // total alive population size
+    let N_0 = S + I + R + D + V;  // max initial population size
+    let N = S + I + R + V;  // total alive population size
     
     infection_rate = 1 / data.get('infection_rate');
     recovery_rate = 1 / data.get('recovery_rate');
@@ -20,37 +20,19 @@ function calc(data) {
     mask_effectiveness = data.get('mask_effectiveness') / 100;
     social_distancing_effectiveness = data.get('social_distancing_effectiveness') / 100;
     vaccine_efficacy = data.get('vaccine_efficacy') / 100;
-
     time = data.get('time');
 
-    S_out = [S];
-    I_out = [I];
-    R_out = [R];
-    D_out = [D];
-    V_out = [V];
-    N_out = [N];
+    let S_out = [S], I_out = [I], R_out = [R], D_out = [D], V_out = [V], N_out = [N];
 
     for (let _ = 0; _ < time; _++) {
-        dS = (-1 * infection_rate * (1 - mask_effectiveness * mask_rate) * (1 - social_distancing_effectiveness * social_distancing_rate) * (I * S) / N + immunity_loss_rate * R - vaccination_rate * S) * Tp;
-        dI = (infection_rate * (1 - mask_effectiveness * mask_rate) * (1 - social_distancing_effectiveness * social_distancing_rate) * (I * S) / N + infection_rate * (1 - mask_effectiveness * mask_rate) * (1 - social_distancing_effectiveness * social_distancing_rate) * (1 - vaccine_efficacy) * (I * V / N) - recovery_rate * I - mortality_rate * I) * Tp;
-        dR = (recovery_rate * I - immunity_loss_rate * R) * Tp;
-        dD = (mortality_rate * I) * Tp;
-        dV = (vaccination_rate * S - (1 - vaccine_efficacy) * infection_rate * (1 - mask_effectiveness * mask_rate) * (1 - social_distancing_effectiveness * social_distancing_rate) * (I * V / N)) * Tp;
-        dN = (-1 * mortality_rate * I) * Tp;
+        S += Math.floor(-1 * infection_rate * (1 - mask_effectiveness * mask_rate) * (1 - social_distancing_effectiveness * social_distancing_rate) * (I * S) / N + immunity_loss_rate * R - vaccination_rate * S) * Tp;
+        I += Math.floor(infection_rate * (1 - mask_effectiveness * mask_rate) * (1 - social_distancing_effectiveness * social_distancing_rate) * (I * S) / N + infection_rate * (1 - mask_effectiveness * mask_rate) * (1 - social_distancing_effectiveness * social_distancing_rate) * (1 - vaccine_efficacy) * (I * V / N) - recovery_rate * I - mortality_rate * I) * Tp;
+        R += Math.floor(recovery_rate * I - immunity_loss_rate * R) * Tp;
+        D += Math.floor(mortality_rate * I) * Tp;
+        V += Math.floor(vaccination_rate * S - (1 - vaccine_efficacy) * infection_rate * (1 - mask_effectiveness * mask_rate) * (1 - social_distancing_effectiveness * social_distancing_rate) * (I * V / N)) * Tp;
+        N += Math.floor(-1 * mortality_rate * I) * Tp;
 
-        S += Math.floor(dS);
-        I += Math.floor(dI);
-        R += Math.floor(dR);
-        D += Math.floor(dD);
-        V += Math.floor(dV);
-        N += Math.floor(dN);
-
-        S_out.push(S);
-        I_out.push(I);
-        R_out.push(R);
-        D_out.push(D);
-        V_out.push(V);
-        N_out.push(N);
+        S_out.push(S); I_out.push(I); R_out.push(R); D_out.push(D); V_out.push(V); N_out.push(N);
     }
     
     return { 'S': S_out, 'I': I_out, 'R': R_out, 'D': D_out, 'V': V_out, 'N': N_out };
