01ace47b129f88ef161b5b0afc49dfdb025137fe
Hi-Im-Simon <75808585+Hi-Im-Simon@users.noreply.github.com>
2022-01-08 14:40:32 +0100

    automated the form.html a bit

--- app/static/javascript/main.js
+++ app/static/javascript/main.js
@@ -7,11 +7,24 @@ for (el of document.querySelectorAll('.row-input')) {
 
     connectFollowingInput(children[0]);
     setChartGenerationProperty(children);
+    setPropertiess(children);
 }
 
 function setChartGenerationProperty(els) {
     for (el of els)
         el.addEventListener('input', function() { generateChart('chart-1', getChartData()) });
+}
+
+function setPropertiess(els) {
+    for (el of els) {
+        if (el.type == 'number') {
+            prev_el = el.previousElementSibling;
+            step = prev_el.step; min = prev_el.min; max = prev_el.max; value = prev_el.value;
+            el.step = step < 1 ? step / 100 : 1;
+            el.min = min; el.max = max; el.value = value;
+            el.placeholder = prev_el.min + ' — ' + prev_el.max;
+        }
+    }
 }
 
 // connect selected input to its following input
